d4a6045415317a45a53677cb91b5c100e4af5661
stsan9 <stsan@ucsd.edu>
2019-10-15 13:49:57 -0700

    code to look at data (not working)

---
+++ viewer.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import json
+
+data = []
+with open('endomondoHR_proper.json') as f:
+    for l in f:
+        data.append(eval(1))
+#
+#dataframe = pd.dataframe.from_dict(data)
+#print(dataframe.head())
