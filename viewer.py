daa59a6481be98f1875601c911f0532d7e6b464b
stsan9 <stsan@ucsd.edu>
2019-10-16 14:02:31 -0700

    updated viewer

--- viewer.py
+++ viewer.py
@@ -2,6 +2,8 @@ import pandas as pd
 import json
 
 data = []
+
+#truncatedData unix command: "head -n 1000 endomondoHR.json > truncatedData.json"
 with open('truncatedData.json') as f:
     for l in f:
         data.append(eval(l))
