ec2fff211ca37dbc2f90855a85ede0c9981703ae
stsan9 <stsan@ucsd.edu>
2019-10-16 14:00:38 -0700

    can make dataframe from just the first 1000 lines

--- viewer.py
+++ viewer.py
@@ -2,9 +2,9 @@ import pandas as pd
 import json
 
 data = []
-with open('endomondoHR_proper.json') as f:
+with open('truncatedData.json') as f:
     for l in f:
-        data.append(eval(1))
-#
-#dataframe = pd.dataframe.from_dict(data)
-#print(dataframe.head())
+        data.append(eval(l))
+
+df = pd.DataFrame.from_dict(data)
+print(df.head())
