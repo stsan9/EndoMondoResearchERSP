12578068183a76e6311fc0dfbf725ea16302158a
Sravya Balasa <sravya.balasa@gmail.com>
2019-10-22 23:31:51 -0700

    Added comments, removed raw_input line to avoid terminal errors

--- visual.py
+++ visual.py
@@ -5,6 +5,7 @@ import json
 import os
 import seaborn as sns
 
+#Truncating currently existing data to open file
 data = []
 path = 'endomondoHR_proper.json'
 os.system('head -n 1000 endomondoHR_proper.json > truncatedData.json')
@@ -13,25 +14,30 @@ with open('truncatedData.json') as f:
     for l in f:
         data.append(eval(l))
 
+#Creating dataframe from dictionary file
 df = pd.DataFrame.from_dict(data)
 df.head()
 
+#Array of all unique sports
 sports = df['sport'].unique()
 print(sports)
 
+#Drop unecessary URLs
 df = df.drop(['id', 'url'], axis = 1)
 df.head(1)
 
+#Array of all unique users
 print(df['userId'].unique())
 
+#Speed and heart rate of first user for a workout
 x = df['speed'][0]
 y = df['heart_rate'][0]
 
+#Speed VS Heart Rate
 plt.scatter(x, y, color='g', s=6)
 plt.xlabel('Speed')
 plt.ylabel('Heart Rate')
 plt.show()
-raw_input('press return to continue')
 
 id_ = 10921915
 
@@ -45,6 +51,7 @@ for i in range(0, 4):
     elif z[i] == 'bike (transport)':
         plt.scatter(x[i], y[i], s=20, c='r')
 
+#Heart Rate vs Speed for two sports
 plt.title('Heart Rate vs Speed Scatter plot (Biking and Running)')
 plt.xlabel('Heart Rate')
 plt.ylabel('Speed')
