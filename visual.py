cfe1ff238f23f8fb3d4374d1ddca039fd28cfafd
Sravya Balasa <sravya.balasa@gmail.com>
2019-10-23 07:27:42 -0700

    can plot two

--- visual.py
+++ visual.py
@@ -20,36 +20,54 @@ df.head()
 
 #Array of all unique sports
 sports = df['sport'].unique()
-print(sports)
+#print(sports)
 
 #Drop unecessary URLs
 df = df.drop(['id', 'url'], axis = 1)
 df.head(1)
 
 #Array of all unique users
-print(df['userId'].unique())
+#print(df['userId'].unique())
 
 #Speed and heart rate of first user for a workout
 x = df['speed'][0]
 y = df['heart_rate'][0]
 
 #Speed VS Heart Rate
-plt.scatter(x, y, color='g', s=6)
-plt.xlabel('Speed')
-plt.ylabel('Heart Rate')
-plt.show()
+#plt.scatter(x, y, color='g', s=6)
+#plt.xlabel('Speed')
+#plt.ylabel('Heart Rate')
+#plt.show()
 
-id_ = 10921915
+#Has various different exercise methods
+id_ = 3905196
+#Prints out all of the users sports
+#for v in dupdf['sport']:
+#    print(v)
 
-x = df[df['userId'] == id_]['speed']
-y = df[df['userId'] == id_]['heart_rate']
-z = df[df['userId'] == id_]['sport']
+#Save only the dataframe where the userid is the id_
+dupdf = df[df.duplicated('userId')]
+dupdf = dupdf.loc[df['userId'] == id_]
 
-for i in range(0, 4):
-    if z[i] == 'bike':
-        plt.scatter(x[i], y[i], s=8, c='b')
-    elif z[i] == 'bike (transport)':
-        plt.scatter(x[i], y[i], s=20, c='r')
+#Grab rows of different exercise methods
+run = df.loc[df['sport'] == 'run']
+bike = df.loc[df['sport'] == 'bike'] 
+
+#Row with run
+x = run['altitude'][30]
+y = run['heart_rate'][30]
+
+#Plot running data
+for i in range(0, len(y)):
+   plt.scatter(x[i], y[i], s=1, c='b')
+
+#Repeat for biking
+x = bike['altitude'][0]
+y = bike['heart_rate'][0]
+
+#Plot running data
+for i in range(0, len(y)):
+   plt.scatter(x[i], y[i], s=1, c='r')
 
 #Heart Rate vs Speed for two sports
 plt.title('Heart Rate vs Speed Scatter plot (Biking and Running)')
